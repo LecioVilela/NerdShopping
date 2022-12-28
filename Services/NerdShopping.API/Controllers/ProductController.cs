328a5d374febe0894744757ffc9ee795a6648298
LecioVilela <lecio.vilela@yahoo.com>
2022-12-27 21:40:37 -0300

    add controller

---
+++ Services/NerdShopping.API/Controllers/ProductController.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NerdShopping.API.Data.ValueObjects;
+using NerdShopping.API.Repository;
+
+namespace NerdShopping.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductRepository _repository;
+
+        public ProductController(IProductRepository repository)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductVO>>> FindAll()
+        {
+            var products = await _repository.FindAll();
+
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductVO>> FindById(long id)
+        {
+            var product = await _repository.FindById(id);
+            if (product is null) return NotFound();
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ProductVO>> Create(ProductVO vo)
+        {
+            if (vo is null) return BadRequest();
+            var product = await _repository.Create(vo);
+            return Ok(product);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<ProductVO>> Update(ProductVO vo)
+        {
+            if (vo is null) return BadRequest();
+            var product = await _repository.Update(vo);
+            return Ok(product);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(long id)
+        {
+            var status = await _repository.Delete(id);
+            if (status is false) return BadRequest();
+            return Ok(status);
+        }
+    }
+}
