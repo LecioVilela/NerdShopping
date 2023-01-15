28112d8e1160094f429a475b74bf9ea674db8177
LecioVilela <lecio.vilela@yahoo.com>
2023-01-15 14:15:27 -0300

    opa

---
+++ FrontEnd/NerdShopping.WEB/Controllers/ProductController.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NerdShopping.WEB.Services.IServices;
+
+namespace NerdShopping.WEB.Controllers
+{
+    [Route("[controller]")]
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+        private readonly ILogger<ProductController> _logger;
+
+        public ProductController(IProductService productService, ILogger<ProductController> logger)
+        {
+            _productService = productService;
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> ProductIndex()
+        {
+            var products = await _productService.FindAllProducts();
+            return View(products);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
